61b7a1b4936c8fb9d555f9fec473f4053ffbf991
Francois ANDRE <francois.andre@obs-mip.fr>
2021-12-05 18:02:19 +0100

    1st version

--- index.js
+++ index.js
@@ -10,14 +10,63 @@
 const init = require('./utils/init');
 const cli = require('./utils/cli');
 const log = require('./utils/log');
+const fs = require("fs");
+const walk = require('walk');
+const FormData = require('form-data');
+const axios = require('axios');
 
 const input = cli.input;
 const flags = cli.flags;
-const { clear, debug } = flags;
+const { clear, debug, folder, service, project } = flags;
 
-(async () => {
-	init({ clear });
-	input.includes(`help`) && cli.showHelp(0);
+(async() => {
+    init({ clear });
+    input.includes(`help`) && cli.showHelp(0);
 
-	debug && log(flags);
+    debug && log(flags);
+    var files = [];
+    var walker = walk.walk(folder, { followLinks: false });
+
+    walker.on('file', function(root, stat, next) {
+        files.push(root + '/' + stat.name);
+        next();
+    });
+
+    walker.on("end", function() {
+        files.forEach(function(file) {
+            var aux = file.split('/');
+            var fileName = aux[aux.length - 1];
+            var folder = '';
+            if (aux.length > 0) {
+                aux.pop();
+                folder = aux.join('/');
+            }
+            const content = fs.readFileSync(file);
+            const form = new FormData();
+            form.append('file', content, fileName);
+
+            axios
+                .post(
+                    service + '/' +
+                    project +
+                    '?folder=' +
+                    folder +
+                    '&filename=' +
+                    fileName,
+                    form, {
+                        headers: form.getHeaders()
+                    }
+                )
+                .then(function() {
+                    console.log('Filename ' + fileName + ' has been uploaded');
+                })
+                .catch(function(error) {
+                    console.error('Filename ' + fileName + ' has NOT been uploaded');
+                    console.error(error);
+                });
+
+
+        });
+    })
+
 })();
--- utils/cli.js
+++ utils/cli.js
@@ -2,45 +2,63 @@ const meow = require('meow');
 const meowHelp = require('cli-meow-help');
 
 const flags = {
-	clear: {
-		type: `boolean`,
-		default: true,
-		alias: `c`,
-		desc: `Clear the console`
-	},
-	noClear: {
-		type: `boolean`,
-		default: false,
-		desc: `Don't clear the console`
-	},
-	debug: {
-		type: `boolean`,
-		default: false,
-		alias: `d`,
-		desc: `Print debug info`
-	},
-	version: {
-		type: `boolean`,
-		alias: `v`,
-		desc: `Print CLI version`
-	}
+    clear: {
+        type: `boolean`,
+        default: true,
+        alias: `c`,
+        desc: `Clear the console`
+    },
+    noClear: {
+        type: `boolean`,
+        default: false,
+        desc: `Don't clear the console`
+    },
+    debug: {
+        type: `boolean`,
+        default: false,
+        alias: `d`,
+        desc: `Print debug info`
+    },
+    version: {
+        type: `boolean`,
+        alias: `v`,
+        desc: `Print CLI version`
+    },
+    folder: {
+        type: `string`,
+        alias: `f`,
+        desc: `local folder of the documentation`,
+        default: `./docs/.vuepress/dist`
+    },
+    service: {
+        type: `string`,
+        alias: `s`,
+        default: `https://services.aeris-data.fr/cdn/documentation/v1_0/upload/`,
+        desc: `Url of the CDN service`
+    },
+    project: {
+        type: `string`,
+        alias: `p`,
+        desc: `Project name`,
+        isRequired: true
+    }
 };
 
 const commands = {
-	help: { desc: `Print help info` }
+    help: { desc: `Print help info` }
 };
 
 const helpText = meowHelp({
-	name: `doc-deploy`,
-	flags,
-	commands
+    name: `doc-deploy`,
+    flags,
+    commands
 });
 
 const options = {
-	inferType: true,
-	description: false,
-	hardRejection: false,
-	flags
+    inferType: true,
+    description: false,
+    hardRejection: false,
+    flags
 };
 
 module.exports = meow(helpText, options);
